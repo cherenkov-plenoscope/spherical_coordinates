2e7015d056d86c87d7c9844a498229890fc08987
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-08-22 13:16:21 +0200

    add doc string to dimensionality

--- spherical_coordinates/dimensionality.py
+++ spherical_coordinates/dimensionality.py
@@ -2,6 +2,23 @@ import numpy as np
 
 
 def _in(x):
+    """
+    Allows to compute input 'x' always as if it is array like
+    while the result of a computation can be returned with the same
+    dimensionality as the dimensionality of the input 'x'.
+    To be used in combination with _out() inside a function.
+
+    Parameters
+    ----------
+    x : array or scalar like
+
+    Returns
+    -------
+    (is_scalar, x) : tuple(bool, array like)
+        The bool 'is_scalar' is for bookkeeping. The output 'x' is array like
+        and has the same content as 'x'.
+    """
+
     x = np.asarray(x)
     is_scalar = False
     if x.ndim == 0:
